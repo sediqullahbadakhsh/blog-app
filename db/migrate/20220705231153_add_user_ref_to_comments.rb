4d2204142d0cb35f48a5f1fc631793aef5b5ad04
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-06 03:54:57 +0430

    add foriegn keys and index

---
+++ db/migrate/20220705231153_add_user_ref_to_comments.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddUserRefToComments < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :comments, :author, foreign_key: { to_table: :users }, null: false
+  end
+end
