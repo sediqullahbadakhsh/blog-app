01e4fa21824bb545471437702d318af8f915dc14
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-06 04:20:04 +0430

    fix linters

--- db/migrate/20220705231931_add_post_ref_to_likes.rb
+++ db/migrate/20220705231931_add_post_ref_to_likes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Post to Likes
 class AddPostRefToLikes < ActiveRecord::Migration[7.0]
   def change
     add_reference :likes, :post, null: false, foreign_key: true
