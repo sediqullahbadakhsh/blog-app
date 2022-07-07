4a6dfbd78fa0b74bfc5f55378be588564a87bffe
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-07 21:24:34 +0430

    fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-    belongs_to :user, foreign_key: :author_id
+  belongs_to :user, foreign_key: :author_id
 
-    after_save :update_comments_counter_for_post
-    
-    private
+  after_save :update_comments_counter_for_post
 
-    def update_comments_counter_for_post
-        post.increment!(:comments_counter)
-    end
+  private
+
+  def update_comments_counter_for_post
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
-    has_many :likes, dependent: :destroy
-    has_many :comments, dependent: :destroy
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-    def most_recent_comments
-      comments.limit(5).order(created_at: :desc)
-    end
-
-    after_save :update_post_counter
-
-    private
-
-    def update_post_counter
-      author.increment!(:posts_counter)
-    end
+  def most_recent_comments
+    comments.limit(5).order(created_at: :desc)
   end
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
+end
