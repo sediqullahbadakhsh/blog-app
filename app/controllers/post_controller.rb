652ffb65e4d96fb88b684bd78bac8d8d7a5b93a7
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-12 02:02:42 +0430

    add post actions

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,11 @@
 class PostController < ApplicationController
+    def index
+        @user = User.find(params[:user_id])
+        @post = Post.where(author_id: @user.id).order(created_at: :desc)
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        @comment = Comment.where(post_id: @post.id).order(created_at: :desc)
+    end
 end
