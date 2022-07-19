e6e2bb167c0cd05aee31d822161fb3fc2735776d
Retky <jose.arredondo.a@outlook.com>
2022-07-18 19:41:58 -0500

    Modify 'user show' fix some specs

--- spec/feature/user_show_page_spec.rb
+++ spec/feature/user_show_page_spec.rb
@@ -1,15 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'User show page test', type: :feature do
-  # I can see the user's profile picture.
-  # I can see the user's username.
-  # I can see the number of posts the user has written.
-  # I can see the user's bio.
-  # I can see the user's first 3 posts.
-  # I can see a button that lets me view all of a user's posts.
-  # When I click a user's post, it redirects me to that post's show page.
-  # When I click to see all posts, it redirects me to the user's post's index page.
-
   it 'I can see the user\'s profile picture.' do
     user = User.create(name: 'Sediq', photo: 'https://unsplash.com/6519861_z.jpg', bio: 'A teacher')
 
@@ -26,10 +17,10 @@ RSpec.describe 'User show page test', type: :feature do
 
   it 'I can see the number of posts the user has written.' do
     user = User.create(name: 'Sediq', photo: 'https://unsplash.com/6519861_z.jpg', bio: 'A teacher')
-    post = Post.create(title: 'My first post', content: 'This is my first post', user_id: user.id)
+    post = Post.create(title: 'My first post', text: 'This is my first post', user_id: user.id)
 
     visit "/users/#{user.id}"
-    expect(page).to have_content('1 post')
+    expect(page).to have_content('Number of posts:1')
   end
 
   it 'I can see the user\'s bio.' do
@@ -41,10 +32,10 @@ RSpec.describe 'User show page test', type: :feature do
 
   it 'I can see the user\'s first 3 posts.' do
     user = User.create(name: 'Sediq', photo: 'https://unsplash.com/6519861_z.jpg', bio: 'A teacher')
-    post = Post.create(title: 'My first post', content: 'This is my first post', user_id: user.id)
-    post = Post.create(title: 'My second post', content: 'This is my second post', user_id: user.id)
-    post = Post.create(title: 'My third post', content: 'This is my third post', user_id: user.id)
-    post = Post.create(title: 'My fourth post', content: 'This is my fourth post', user_id: user.id)
+    post = Post.create(title: 'My first post', text: 'This is my first post', user_id: user.id)
+    post = Post.create(title: 'My second post', text: 'This is my second post', user_id: user.id)
+    post = Post.create(title: 'My third post', text: 'This is my third post', user_id: user.id)
+    post = Post.create(title: 'My fourth post', text: 'This is my fourth post', user_id: user.id)
 
     visit "/users/#{user.id}"
     expect(page).to_not have_content('My first post')
@@ -55,35 +46,35 @@ RSpec.describe 'User show page test', type: :feature do
 
   it 'I can see a button that lets me view all of a user\'s posts.' do
     user = User.create(name: 'Sediq', photo: 'https://unsplash.com/6519861_z.jpg', bio: 'A teacher')
-    post = Post.create(title: 'My first post', content: 'This is my first post', user_id: user.id)
+    post = Post.create(title: 'My first post', text: 'This is my first post', user_id: user.id)
 
     visit "/users/#{user.id}"
-    expect(page).to have_button('View all posts')
+    expect(page).to have_button('See all posts')
   end
 
   it 'When I click a user\'s post, it redirects me to that post\'s show page.' do
     user = User.create(name: 'Sediq', photo: 'https://unsplash.com/6519861_z.jpg', bio: 'A teacher')
-    post = Post.create(title: 'My first post', content: 'This is my first post', user_id: user.id)
+    post = Post.create(title: 'My first post', text: 'This is my first post', user_id: user.id)
 
     visit "/users/#{user.id}"
-    click_on 'View all posts'
-    expect(page).to have_current_path("/users/#{user.id}/posts")
+    click_on 'View Post'
+    expect(page).to have_current_path("/users/#{user.id}/posts/#{post.id}")
   end
 
   it 'When I click to see all posts, it redirects me to the user\'s post\'s index page.' do
     user = User.create(name: 'Sediq', photo: 'https://unsplash.com/6519861_z.jpg', bio: 'A teacher')
-    post = Post.create(title: 'My first post', content: 'This is my first post', user_id: user.id)
+    post = Post.create(title: 'My first post', text: 'This is my first post', user_id: user.id)
 
     visit "/users/#{user.id}/posts"
-    click_on 'My first post'
-    expect(page).to have_current_path("/users/#{user.id}/posts/#{post.id}")
+    click_on 'See all posts'
+    expect(page).to have_current_path("/users/#{user.id}/posts")
   end
 
   it 'I can see a button that lets me view all of a user\'s posts.' do
     user = User.create(name: 'Sediq', photo: 'https://unsplash.com/6519861_z.jpg', bio: 'A teacher')
-    post = Post.create(title: 'My first post', content: 'This is my first post', user_id: user.id)
+    post = Post.create(title: 'My first post', text: 'This is my first post', user_id: user.id)
 
-    visit "/users/#{user.id}/posts"
-    expect(page).to have_button('View all posts')
+    visit "/users/#{user.id}"
+    expect(page).to have_button('See all posts')
   end
 end
