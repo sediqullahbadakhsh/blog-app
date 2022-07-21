ee02c81ced1ce50feb451e2a0422babd4f0b2465
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-21 23:26:24 +0430

    api end point for a users-post

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -6,6 +6,11 @@ module Api
                 posts = Post.where(user_id: user_id)
                 render json: {status: 'SUCCESS', message: 'Loaded Posts', data: posts}, status: :ok
             end
+            def user_comments
+                post_id = params[:post_id]
+                comments = Comment.where(post_id: post_id)
+                render json: {status: 'SUCCESS', message: 'Loaded Posts', data: comments}, status: :ok
+            end
         end
     end
 end
