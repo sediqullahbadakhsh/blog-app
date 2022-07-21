4403a59192b3f8c746d87833162c2fd68de86b1f
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-21 14:49:58 +0430

    handle decrement comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,14 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_save :update_comments_counter_for_post
+  after_save :delete_comments_counter_for_post
 
   private
 
   def update_comments_counter_for_post
     post.increment!(:comments_counter)
+  end
+  def delete_comments_counter_for_post
+    post.decrement!(:comments_counter)
   end
 end
